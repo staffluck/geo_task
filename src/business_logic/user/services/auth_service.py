593c30fb8aa3e6a1fdb3bda98534faac4a1d6863
Arseny Leontev <arseny.leontev@gmail.com>
2023-01-18 00:55:19 +0400

    Сервис авторизации

--- src/business_logic/user/services/auth_service.py
+++ src/business_logic/user/services/auth_service.py
@@ -1,5 +1,7 @@
+from src.business_logic.user.protocols.security import IHashManager, IJWTManager
 
 
 class AuthService:
-    def __init__(self, jwt_manager: ) -> None:
-        
+    def __init__(self, jwt_manager: IJWTManager, hash_manager: IHashManager) -> None:
+        self.jwt_manager = jwt_manager
+        self.hash_manager = hash_manager
