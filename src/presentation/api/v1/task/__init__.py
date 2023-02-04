dca89d3af91a04668e50b95fa0304419a4d919b1
Arseny Leontev <arseny.leontev@gmail.com>
2023-02-04 14:10:44 +0400

    Проставка tags

--- src/presentation/api/v1/task/__init__.py
+++ src/presentation/api/v1/task/__init__.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 from src.presentation.api.v1.task.task import router
 
 task_router = APIRouter(prefix="/task")
-task_router.include_router(router)
+task_router.include_router(router, tags=["Task"])
--- src/presentation/api/v1/user/__init__.py
+++ src/presentation/api/v1/user/__init__.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 from src.presentation.api.v1.user.auth import router as auth_router
 
 user_router = APIRouter(prefix="/user")
-user_router.include_router(auth_router)
+user_router.include_router(auth_router, tags=["user"])
