1b497a1c7e569b8206dfffbfa454d69886268181
Arseny Leontev <arseny.leontev@gmail.com>
2023-02-04 15:55:03 +0400

    Вынос функций создания объектов для DI из роутеров в STUB

--- src/presentation/api/app.py
+++ src/presentation/api/app.py
@@ -1,4 +1,3 @@
-import uvicorn
 from fastapi import FastAPI
 
 from src.business_logic.common.exceptions import (
@@ -6,27 +5,40 @@ from src.business_logic.common.exceptions import (
     ObjectAlreadyExistsError,
     ObjectNotFoundError,
 )
-from src.config import ServerSettings
+from src.business_logic.task.services.task_service import TaskService
+from src.business_logic.user.services.auth_service import AuthService
 from src.infrastructure.data_access.postgresql.tables.mappers import map_tables
 from src.presentation.api.exception_handler import (
     application_error_handler,
     object_already_exists_error_handler,
     object_not_found_error_handler,
 )
+from src.presentation.api.v1.depends import get_auth_service, get_task_service
 from src.presentation.api.v1.routers import router
+
+
+def setup_exception_handlers(app: FastAPI) -> None:
+    app.add_exception_handler(ApplicationError, application_error_handler)
+    app.add_exception_handler(ObjectNotFoundError, object_not_found_error_handler)
+    app.add_exception_handler(
+        ObjectAlreadyExistsError, object_already_exists_error_handler
+    )
+
+
+def setup_dependency(app: FastAPI) -> None:
+    app.dependency_overrides[TaskService] = get_task_service
+    app.dependency_overrides[AuthService] = get_auth_service
 
 
 def setup_app() -> FastAPI:
     app = FastAPI(debug=True)
     app.include_router(router)
 
+    setup_exception_handlers(app)
+    setup_dependency(app)
+
     map_tables()
 
-    app.add_exception_handler(ApplicationError, application_error_handler)
-    app.add_exception_handler(ObjectNotFoundError, object_not_found_error_handler)
-    app.add_exception_handler(
-        ObjectAlreadyExistsError, object_already_exists_error_handler
-    )
     return app
 
 
