2ddd88198c090854099430263c70c8c942292341
Arseny Leontev <arseny.leontev@gmail.com>
2023-02-18 22:19:17 +0400

    Базовая схема для paginated ответов

--- src/presentation/schemas/common.py
+++ src/presentation/schemas/common.py
@@ -1,6 +1,21 @@
+from typing import Generic, TypeVar
+
 from pydantic import BaseModel
+from pydantic.generics import GenericModel
+
+from src.business_logic.common.dto.base import DTO
 
 
 class LimitOffsetQuerySchema(BaseModel):
     limit: int = 100
     offset: int = 0
+
+
+Model = TypeVar("Model", bound=DTO)
+
+
+class BasePaginate(GenericModel, Generic[Model]):
+    total: int
+    limit: int
+    offset: int
+    data: list[Model]
