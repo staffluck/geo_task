0ae5dacdd54888e457d6eec88c5b7c95e42e81d1
Arseny Leontev <arseny.leontev@gmail.com>
2023-02-18 23:29:09 +0400

    Общий ответ для AccessDeniedError

--- src/presentation/api/openapi_responses/common.py
+++ src/presentation/api/openapi_responses/common.py
@@ -1,3 +1,23 @@
 from typing import Any
 
+from fastapi import status
+
+from src.business_logic.common.exceptions import AccessDeniedError
+from src.presentation.api.openapi_responses.generator import generate_example_schema
+
 RESPONSE_TYPE = dict[int | str, dict[str, Any]]
+
+
+access_denied_response = {
+    "description": "Forbidden",
+    "content": {
+        "application/json": {
+            "examples": {
+                "access_denied": {
+                    "summary": "Нет доступа",
+                    "value": generate_example_schema(AccessDeniedError()),
+                }
+            }
+        }
+    },
+}
