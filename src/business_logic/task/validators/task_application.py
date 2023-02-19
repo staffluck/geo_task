7c3729ca01c26db8fb47d33138f7c79f566e931a
Arseny Leontev <arseny.leontev@gmail.com>
2023-02-19 14:29:25 +0400

    Опечатка + ненужный импорт

--- src/business_logic/task/validators/task_application.py
+++ src/business_logic/task/validators/task_application.py
@@ -1,5 +1,3 @@
-import re
-
 from src.business_logic.common.validators import ValidationError, length_validator
 
 TEXT_MIN_LENGTH = 10
@@ -13,6 +11,3 @@ text_length_validation_error = ValidationError(
 def validate_text(text: str) -> None:
     if not length_validator(text, TEXT_MIN_LENGTH, TEXT_MAX_LENGTH):
         raise text_length_validation_error
-
-
-def 
