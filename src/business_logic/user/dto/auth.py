7348c730b70b60c3908773732c81e89e1acad9a7
Arseny Leontev <arseny.leontev@gmail.com>
2023-02-19 14:10:00 +0400

    Валидация полей first_name и last_name

--- src/business_logic/user/dto/auth.py
+++ src/business_logic/user/dto/auth.py
@@ -1,7 +1,11 @@
 from pydantic import EmailStr, Field, validator
 
 from src.business_logic.common.dto.base import DTO
-from src.business_logic.user.validators.password import validate_password
+from src.business_logic.user.validators.validators import (
+    validate_first_name,
+    validate_last_name,
+    validate_password,
+)
 
 
 class BaseUser(DTO):
@@ -17,6 +21,16 @@ class UserCreate(BaseUser):
     def validate_raw_password(cls, password: str) -> str:  # noqa: N805
         validate_password(password)
         return password
+
+    @validator("first_name", pre=True)
+    def validate_first_name(cls, first_name: str) -> str:  # noqa: N805
+        validate_first_name(first_name)
+        return first_name
+
+    @validator("last_name", pre=True)
+    def validate_last_name(cls, last_name: str) -> str:  # noqa: N805
+        validate_last_name(last_name)
+        return last_name
 
 
 class UserSignin(DTO):
