c497f096fdb9bbf381f1eaa6c9b3218ec7924232
Arseny Leontev <arseny.leontev@gmail.com>
2023-03-04 11:43:24 +0400

    Парсинг команд

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,12 @@
+import argparse
+
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("src.presentation.api.app:app")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--import-cities", "-ic", action="store_true")
+    args = parser.parse_args()
+    if args.import_cities:
+        import_cities()
+    else:
+        uvicorn.run("src.presentation.api.app:app")
